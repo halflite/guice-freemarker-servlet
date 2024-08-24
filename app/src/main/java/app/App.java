43de86d0e419910ea09ee95af6c9874fdbfc587a
ShingoHayashibara <shingo.hayashibara@digilogi.com>
2024-08-24 12:30:07 +0900

    ログと変数の設定追加

--- app/src/main/java/app/App.java
+++ app/src/main/java/app/App.java
@@ -1,8 +1,14 @@
 package app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /** App Main */
 public class App {
+  /** logger */
+  private final static Logger LOG = LoggerFactory.getLogger(App.class);
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    LOG.info("Hello World!");
   }
 }
